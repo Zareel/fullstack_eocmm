7947fe4604b93455dded3418ac081a5949823f3a
Zreel <zareelbwd@gmail.com>
2026-03-25 14:43:31 +0530

    middlewares

---
+++ src/middlewares/authMiddlewares.js
@@ -0,0 +1,57 @@
+import JWT from "jsonwebtoken"
+import config from "../config/config.js";
+import User from "../models/userSchema.js";
+import AuthRoles from "../utils/AuthRoles.js";
+
+
+// isLoggedIn
+export const isLoggedIn = async(req, res, next) =>{
+    try{
+        const {token} = req.cookies;
+        // if no token send msg
+        if(!token){
+            res.status(404).json({
+                success:false,
+                message:"Un-authorized user"
+            })
+        }
+        // if token found
+        const decoded = JWT.verify(token, config.JWT_SECRET)
+
+        req.user =  decoded;
+        next()
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success: false,
+            message:"Error in middleware",
+            error
+        })
+    }
+}
+
+// isAdmin
+export const isAdmin = async(req, res, next) =>{
+    try{
+        const user = await User.findById(req.user._id)
+        if(user.role.toLowerCase() !== AuthRoles.ADMIN.toLowerCase()){
+            res.status(400).json({
+                success:false,
+                message:"You are not authorized to access this page"
+            })
+        }else{
+            next()
+        }
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            success:false,
+            message:"Error in admin middleware",
+            error
+        })
+    }
+
+}
+
