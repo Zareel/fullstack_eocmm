562e02e3a325d3816d5cf83fc0dd4c1d8cc2e109
Zreel <zareelbwd@gmail.com>
2026-03-28 15:03:31 +0530

    test route

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -17,5 +17,12 @@ router.post("/logout", logout)
 // test route
 router.get("/test",isLoggedIn, isAdmin, testController)
 
+// protected route (to check the authenticated user)
+// if the user logged in we get true in the frontend so that the user can access the dashboard or more
+router.get("/user-auth", isLoggedIn, (req, res) =>{
+    res.status(200).send({ok:true})
+})
+
+
 
 export default router
