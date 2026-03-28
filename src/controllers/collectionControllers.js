3fc8484260eadceffa44f742fce013395cd4f830
Zreel <zareelbwd@gmail.com>
2026-03-28 15:02:17 +0530

    collection CRUD

--- src/controllers/collectionControllers.js
+++ src/controllers/collectionControllers.js
@@ -1,5 +1,109 @@
 import Collection from "../models/collectionSchema.js";
+import slugify from "slugify";
 
+
+
+
+// get all collection
+export const getAllCollection = async (req, res) => {
+  try {
+    const collection = await Collection.find();
+    if (!collection) {
+      return res.status(404).json({
+        success: false,
+        message: "Collections not found",
+      });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Successfully fetched all collection",
+      count: collection.length,
+      collection,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      success: false,
+      message: `Error in getting all collection ${error}`,
+      error,
+    });
+  }
+};
+
+// delete collection
+export const deleteCollection = async(req, res) => {
+  try{
+    const {id} = req.params
+   const collectionToDelete =  await Collection.findByIdAndDelete(id)
+
+   if(!collectionToDelete){
+    return res.status(404).json({
+      success:false,
+      message:"Collection not found"
+    })
+   }
+
+   res.status(200).json({
+    success:true,
+    message:"Collection has been deleted succesfully",
+    collectionToDelete
+   })
+
+
+  }catch(error){
+    console.log(error)
+    res.status(500).json({
+      success:false,
+      message:"Error in delete collection",
+      error
+    })
+  }
+}
+
+// get single collection
+export const singleCollection = async(req, res) => {
+  try{
+    const singleCollection = await Collection.findOne({id:req.params._id})
+    res.status(200).json({
+      success:true,
+      message:"Successfully fetched single collection",
+      singleCollection
+    })
+
+  }catch(error){
+    console.log(error)
+    res.status(500).json({
+      success:false,
+      message:"Error in getting single collection",
+      error
+    })
+  }
+}
+
+// update collection
+export const updateCollection = async(req, res) =>{
+  const {id} = req.params
+  const {name} = req.body
+  try{
+    const collection = await Collection.findByIdAndUpdate(id, {name, slug:slugify(name)}, {new:true})
+    res.status(200).json({
+      success:true,
+      message:"Collection name has been updated successfully",
+      collection
+    })
+
+  }catch(error){
+    console.log(error)
+    res.status(500).json({
+      success: false,
+      message:`Error in updating the collection ${error}`,
+      error
+
+    })
+  }
+}
+
+// Create collection
 export const createCollection = async (req, res) => {
   try {
     // get the info from the frontend
@@ -22,7 +126,7 @@ export const createCollection = async (req, res) => {
       });
     }
     // else create new collection
-    const collection = await Collection.create({ name });
+    const collection = await Collection.create({ name, slug:slugify(name) });
     // send success response
     res.status(200).json({
       status: true,
@@ -38,3 +142,5 @@ export const createCollection = async (req, res) => {
     });
   }
 };
+
+
--- src/routes/collectionRoutes.js
+++ src/routes/collectionRoutes.js
@@ -1,10 +1,30 @@
 import express from "express";
-import { createCollection } from "../controllers/collectionControllers.js";
+import {
+  createCollection,
+  deleteCollection,
+  getAllCollection,
+  singleCollection,
+  updateCollection,
+} from "../controllers/collectionControllers.js";
+import { isAdmin, isLoggedIn } from "../middlewares/authMiddlewares.js";
 
 const router = express.Router();
 
 // routes
-// createCollection | method: post
-router.post("/create-collection", createCollection);
+// createCollection | method: post | /api/v1/collection/create-collection
+router.post("/create-collection",createCollection);
+
+// get all collection
+router.get("/get-all-collection", getAllCollection);
+
+// delete collection
+router.delete("/delete-collection/:id", deleteCollection)
+
+// single collection
+router.get("/single-collection/:id",isLoggedIn, singleCollection)
+
+// update collection
+router.put("/update-collection/:id", updateCollection)
+
 
 export default router;
