c4f98ff14b3963e4f62e5ae48ed3c941211f042c
Zreel <zareelbwd@gmail.com>
2026-04-16 15:38:36 +0530

    create product api

--- src/controllers/productControllers.js
+++ src/controllers/productControllers.js
@@ -5,19 +5,12 @@ import slugify from "slugify";
 export const createProduct = async (req, res) => {
   try {
     // get info from frontend. As we have installed formidable we will grab the data from req.fields instead of req.body
-    const { name, description, price, collection, quantity } =
-      req.fields;
+    const { name, description, price, collection, quantity } = req.fields;
     // get photo from req.files
     const { photo } = req.files;
 
     // validation
-    if (
-      !name ||
-      !description ||
-      !price ||
-      !collection ||
-      !quantity
-    ) {
+    if (!name || !description || !price || !collection || !quantity) {
       return res.status(400).json({
         success: false,
         message: "Pls fill all the fields",
@@ -25,7 +18,7 @@ export const createProduct = async (req, res) => {
     }
 
     // photo validation
-    if (!photo && photo.size > 1000000) {
+    if (!photo || photo.size > 1000000) {
       return res.status(400).json({
         success: false,
         message: "Photo is required and it cannot be more than 1MB",
@@ -37,7 +30,7 @@ export const createProduct = async (req, res) => {
 
     // if exist send response
     if (existingProduct) {
-      return res.status(200).json({
+      return res.status(409).json({
         success: false,
         message: "Product already exists",
       });
@@ -46,7 +39,7 @@ export const createProduct = async (req, res) => {
     // if not exists create new product
     const product = new Product({
       ...req.fields,
-      slug: slugify(name),
+      slug: slugify(name, { lower: true }),
     });
     if (photo) {
       product.photo.data = fs.readFileSync(photo.path);
@@ -54,16 +47,14 @@ export const createProduct = async (req, res) => {
     }
 
     // save the product
-    await product.save()
+    await product.save();
 
     // send success message
     res.status(200).json({
-        success:true,
-        message:"New product has been created successfully",
-        product
-    })
-
-
+      success: true,
+      message: "New product has been created successfully",
+      product,
+    });
   } catch (error) {
     console.log(error);
     res.status(500).json({
--- src/models/productSchema.js
+++ src/models/productSchema.js
@@ -36,7 +36,7 @@ const productSchema = new mongoose.Schema(
     shipping: {
       type: Boolean,
       required: true,
-      default:"false"
+      default:false
     },
   },
   { timestamps: true },
