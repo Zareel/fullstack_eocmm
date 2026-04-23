f9bec675edf4ee604c8c7211f4cf898d3b5f814f
Zreel <zareelbwd@gmail.com>
2026-04-23 15:13:41 +0530

    WIP Update product

--- client/src/pages/admin/AdminProducts.jsx
+++ client/src/pages/admin/AdminProducts.jsx
@@ -2,9 +2,11 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { toast } from "sonner";
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 const AdminProducts = () => {
   const [products, setProducts] = useState([]);
+  const params = useParams();
 
   // get all products
   const getProducts = async () => {
@@ -12,7 +14,7 @@ const AdminProducts = () => {
       const { data } = await axios.get("/api/v1/product/get-all-products");
       // console.log(data)
       if (data?.success) {
-        setProducts(data.products);
+        setProducts(data?.products);
       } else {
         toast.error(data?.message);
       }
@@ -25,13 +27,33 @@ const AdminProducts = () => {
     getProducts();
   }, []);
 
+  const deleteProduct = async (id) => {
+    try {
+      const { data } = await axios.delete(
+        `/api/v1/product/delete-product/${id}`,
+      );
+      if (data?.success) {
+        getProducts();
+        toast.success(data?.message);
+      } else {
+        toast.error(data?.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while deleting the product");
+    }
+  };
+
   return (
     <div className="min-h-screen">
       <h1 className="text-5xl text-cyan-300 font-bold pb-10">Products</h1>
       <div className="flex gap-10 flex-wrap">
         {products.map((item) => {
           return (
-            <Link  key={item._id} className="w-xs rounded-md shadow-md bg-gray-50 text-gray-900">
+            <div
+              key={item._id}
+              className="w-xs rounded-md shadow-md bg-gray-50 text-gray-900"
+            >
               <img
                 src={`/api/v1/product/product-photo/${item._id}`}
                 alt=""
@@ -39,23 +61,40 @@ const AdminProducts = () => {
               />
               <div className="flex flex-col justify-between p-2 space-y-4">
                 <div className="space-y-2 flex justify-between">
-                <div>
-                      <h2 className="text-3xl font-semibold tracking-wide">
-                    {item.name}
-                  </h2>
-                  <p className="text-gray-400">
-                    {item.description}
-                  </p>
-                </div>
-                  <p>Rs.{item.price}</p>
+                  <div>
+                    <h2 className="text-3xl font-semibold tracking-wide">
+                      {item.name}
+                    </h2>
+                    <p className="text-gray-400">{item.description}</p>
+                  </div>
+                  <div>
+                    <p>Rs.{item.price}</p>
+                    <p className="text-gray-600 font-semibold text-sm">
+                      Stock:
+                      <span className="text-lg text-black">
+                        {item.quantity}
+                      </span>{" "}
+                    </p>
+                  </div>
                 </div>
                 <div className="flex gap-4 justify-center mb-2">
-                    <Link to={`/dashboard/admin/product/${item.slug}`} className="px-8 py-3 font-semibold rounded bg-cyan-800 dark:text-gray-100">Update</Link>
-                    <button className="px-8 py-2.5 font-semibold rounded bg-cyan-800 dark:text-gray-100">Delete</button>
+                  <Link
+                    to={`/dashboard/admin/product/${item.slug}`}
+                    className="px-8 py-3 font-semibold rounded bg-cyan-800 hover:bg-amber-500 dark:text-gray-100"
+                  >
+                    Update
+                  </Link>
+                  <button
+                    onClick={() => {
+                      deleteProduct(item._id);
+                    }}
+                    className="px-8 py-2.5 font-semibold cursor-pointer rounded bg-cyan-800 hover:bg-red-600 dark:text-gray-100"
+                  >
+                    Delete
+                  </button>
                 </div>
-                
               </div>
-            </Link>
+            </div>
           );
         })}
       </div>
---
+++ client/src/pages/admin/UpdateProduct.jsx
@@ -0,0 +1,188 @@
+import React, { useState, useEffect } from "react";
+import { toast } from "sonner";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { Select } from "antd";
+const { Option } = Select;
+
+const UpdateProduct = () => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [collection, setCollection] = useState("");
+  const [collections, setCollections] = useState([]);
+  const [quantity, setQuantity] = useState("");
+  const [shipping, setShipping] = useState(false);
+  const [photo, setPhoto] = useState("");
+  const navigate = useNavigate();
+
+  // get Collection
+  const getCollection = async () => {
+    try {
+      const { data } = await axios.get("/api/v1/collection/get-all-collection");
+      if (data?.success) {
+        setCollections(data?.collection);
+      }
+      // console.log(collections)
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong in getting collection");
+    }
+  };
+
+  useEffect(() => {
+    getCollection();
+  }, []);
+
+  // create Product
+  const createProduct = async (e) => {
+    try {
+      e.preventDefault();
+      const productData = new FormData();
+      productData.append("name", name);
+      productData.append("description", description);
+      productData.append("price", price);
+      productData.append("collection", collection);
+      productData.append("quantity", quantity);
+      productData.append("photo", photo);
+      const { data } = await axios.post(
+        "/api/v1/product/create-product",
+        productData,
+      );
+      if (data?.success) {
+        toast.success(data.message);
+        navigate("/dashboard/admin/products");
+      } else {
+        toast.error(data?.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong while creating the product");
+    }
+  };
+  return (
+    <div className="min-h-screen">
+      <h1 className="text-5xl text-cyan-300 font-bold">Update Products</h1>
+      <form onSubmit={createProduct}>
+        <div className="my-6">
+          <Select
+            placeholder="Select a collection"
+            size="large"
+            value={collection || undefined}
+            showSearch
+            className="w-auto md:min-w-125 mx-auto"
+            onChange={(value) => setCollection(value)}
+          >
+            {collections.map((item) => {
+              return (
+                <Option key={item._id} value={item._id}>
+                  {item.name}
+                </Option>
+              );
+            })}
+          </Select>
+        </div>
+        {/* get photo */}
+        <div className="mt-6">
+          <label
+            htmlFor="upload-images"
+            className="cursor-pointer bg-gray-800 px-6 py-2 rounded-sm"
+          >
+            {photo ? photo.name : "Upload photo"}
+
+            <input
+              id="upload-images"
+              type="file"
+              hidden
+              accept="image/*"
+              onChange={(e) => setPhoto(e.target.files[0])}
+            />
+          </label>
+        </div>
+        <div className="my-6">
+          {photo && (
+            <div>
+              <img
+                src={URL.createObjectURL(photo)}
+                alt="product-photo"
+                className="w-64"
+              />
+            </div>
+          )}
+        </div>
+        <div>
+          {/* name */}
+          <div className="my-4">
+            <input
+              type="text"
+              value={name}
+              placeholder="Product Name"
+              className="border-none outline-none bg-gray-800 tracking-wider w-auto md:min-w-125  mx-auto px-4 py-2 rounded-sm"
+              onChange={(e) => {
+                setName(e.target.value);
+              }}
+            />
+          </div>
+          {/* description */}
+          <div>
+            <textarea
+              type="text"
+              value={description}
+              placeholder="Description"
+              className="border-none outline-none bg-gray-800 tracking-wider w-auto md:min-w-125  mx-auto px-4 py-2 rounded-sm"
+              onChange={(e) => {
+                setDescription(e.target.value);
+              }}
+            ></textarea>
+          </div>
+          {/* price */}
+          <div className="my-2">
+            <input
+              type="number"
+              value={price}
+              placeholder="Price"
+              className="border-none outline-none bg-gray-800 tracking-wider w-auto md:min-w-125 mx-auto px-4 py-2 rounded-sm"
+              onChange={(e) => {
+                setPrice(e.target.value);
+              }}
+            />
+          </div>
+          {/* quantity */}
+          <div className="my-4">
+            <input
+              type="number"
+              value={quantity}
+              placeholder="Stock"
+              className="border-none outline-none bg-gray-800 tracking-wider w-auto md:min-w-125 mx-auto px-4 py-2 rounded-sm"
+              onChange={(e) => setQuantity(e.target.value)}
+            />
+          </div>
+          {/* Shipped */}
+          <div className="my-4">
+            <Select
+              size="large"
+              placeholder="Select Shipping"
+              onChange={(value) => setShipping(value)}
+            >
+              <Option value={true}>Yes</Option>
+              <Option value={false}>No</Option>
+            </Select>
+          </div>
+          <div className="flex justify-center items-center">
+            <button
+              type="submit"
+              className="relative px-8 py-4 ml-4 overflow-hidden font-semibold rounded dark:bg-gray-800 dark:text-gray-50"
+            >
+              Save Updates
+              <span className="absolute top-0 right-0 px-5 py-1 text-xs tracking-wider text-center uppercase whitespace-no-wrap origin-bottom-left transform rotate-45 -translate-y-full translate-x-1/3 dark:bg-cyan-600 text-transparent ">
+                new
+              </span>
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default UpdateProduct;
